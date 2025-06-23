8f35a2b2979ba7956c29f6338e4e25966bfe4d4e
kaminokazuya <zeal24360186@fsgcl.jp>
2025-06-23 10:10:06 +0900

    練習問題11-1 11-2

--- src/デフォルト/CurveEnemy.java
+++ src/デフォルト/CurveEnemy.java
@@ -13,4 +13,10 @@ public class CurveEnemy extends Enemy {
 			x--;
 		}
 	}
+	public void draw(MyFrame f) {
+		f.setColor(50, 50, 50);
+		f.fillOval(x,y,30,30);
+		f.setColor(250, 250, 250);
+		f.fillOval(x+5,y,20,30);
+	}
 }
--- src/デフォルト/StraightEnemy.java
+++ src/デフォルト/StraightEnemy.java
@@ -4,4 +4,10 @@ public class StraightEnemy extends Enemy {
 	public StraightEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
+	public void draw(MyFrame f) {
+		f.setColor(50, 50, 50);
+		f.fillRect(x+8,y,14,15);
+		f.setColor(50, 50, 50);
+		f.fillRect(x+10,y+15,10,15);
+	}
 }
