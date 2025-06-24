f9fac66c40b20a52f5e11eb4b632cd4a51783b30
kaminokazuya <zeal24360186@fsgcl.jp>
2025-06-24 11:19:31 +0900

    教科書13.2

--- src/デフォルト/EnemyBase.java
+++ src/デフォルト/EnemyBase.java
@@ -9,19 +9,19 @@ public class EnemyBase extends Enemy {
 		super.move();
 		if (x>300) vx=-1;
 		if (x<100) vx=1;
-		if (Math.random()<0.05) {
+		if (Math.random()<0.01) {
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
 		}
-		if (Math.random()<0.05) {
+		if (Math.random()<0.01) {
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,1));
 		}
-		if (Math.random()<0.05) {
+		if (Math.random()<0.01) {
 			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
 		}
-		if (Math.random()<0.05) {
+		if (Math.random()<0.01) {
 			GameWorld.enemies.add(new DropEnemy(x,y,0,2));
 		}
-		if (Math.random()<0.05) {
+		if (Math.random()<0.01) {
 			GameWorld.enemies.add(new CurveEnemy(x,y,0,1));
 		}
 	}
